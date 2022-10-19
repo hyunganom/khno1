e871fe292b44b797bf9a2f84f23391cbf2c5ce72
이현재 <104957944+hyunganom@users.noreply.github.com>
2022-10-20 00:03:00 +0900

    Update 상품이미지첨부파일(product_attachment).sql

--- DB Table/상품이미지첨부파일(product_attachment).sql
+++ DB Table/상품이미지첨부파일(product_attachment).sql
@@ -4,5 +4,11 @@
 create table product_attachment(
 product_origin_no number references product(product_no) on delete cascade,
 product_attachment_no number references attachment(attachment_no) on delete cascade
+ 
+ -- 뷰생성 권한
+GRANT CREATE view to semi;
+
+ --뷰생성
+CREATE view product_attachment_view as SELECT * FROM product_attachment p INNER JOIN attachment a on a.attachment_no=p.product_attachment_no;
 );
 
